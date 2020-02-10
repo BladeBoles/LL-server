8bb2105ac75ab0e696b8a67756a0c1a2b438e3ca
Blade Boles <bladerboles@gmail.com>
2020-02-10 13:10:46 -0700

    First server-side commit.  Not much more than boilerplate.

---
+++ test/app.spec.js
@@ -0,0 +1,10 @@
+
+const app = require('../src/app');
+
+describe('App', () => {
+  it('GET /api responds with 200 containing "Hello, api!"', () => {
+    return supertest(app)
+      .get('/api')
+      .expect(200, 'Hello, world!');
+  });
+});
