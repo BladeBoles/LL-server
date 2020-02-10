f30bf0fdad6e10bda3284d4f33486cecbdb013ac
Blade Boles <bladerboles@gmail.com>
2020-02-10 14:03:24 -0700

    Created database and began writing migration scripts, but paused on that.  Created a fixtures file for generating examples.  Created example for currently reading page.

---
+++ src/fixtures.js
@@ -0,0 +1,37 @@
+function makeCurrentlyReadingArray () {
+  return [
+    {
+      "media_name": "Moby Dick",
+      "current_progress": 360,
+      "finished": false,
+      "media_type": "Book",
+      "date_started": "02-03-20",
+      "date_finished": "",
+      "author": "Herman Melville",
+      "url": "https://en.wikipedia.org/wiki/Moby-Dick",
+      "notes": "A real whale of a tale."
+    },
+    {
+      "media_name": "Little Women",
+      "current_progress": 180,
+      "finished": false,
+      "media_type": "Audiobook",
+      "date_started": "10-31-19",
+      "date_finished": "",
+      "author": "Louisa May Alcott",
+      "url": "https://en.wikipedia.org/wiki/Little-Women",
+      "notes": "Was the movie better than the book?"
+    },
+    {
+      "media_name": "How Much Sleep Do You Need?",
+      "current_progress": 15,
+      "finished": false,
+      "media_type": "Article",
+      "date_started": "02-09-20",
+      "date_finished": "",
+      "author": "Willie Nelson",
+      "url": "https://www.groovytunes.com/sleep",
+      "notes": "Six hours isn't enough for anyone!"
+    }
+  ];
+}
