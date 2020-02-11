2cbe6aa7fd0ae754b6eaf4e2db956240f8229a7d
Blade Boles <bladerboles@gmail.com>
2020-02-11 09:52:16 -0700

    Created migrations and seeds for currently_reading database.  Next going to get these to display on the client.

---
+++ migrations/001.undo.create_currently_reading.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS currently_reading;
---
+++ seeds/seed.currently_reading.sql
@@ -0,0 +1,27 @@
+INSERT INTO currently_reading (media_name, current_progress, finished, media_type, date_started, date_finished, author, media_url, notes)
+VALUES
+(
+    'Moby Dick', 360, false, 'Book', '02-03-20', null, 'Herman Melville', 'https://en.wikipedia.org/wiki/Moby-Dick', 'A real whale of a tale.'
+),
+(
+    'Little Women',
+    180,
+    false,
+    'Audiobook',
+    '10-31-19',
+    null,
+    'Louisa May Alcott',
+    'https://en.wikipedia.org/wiki/Little-Women',
+    'Was the movie better than the book?'
+),
+(
+    'How Much Sleep Do You Need?',
+    15,
+    false,
+    'Article',
+    '02-09-20',
+    null,
+    'Willie Nelson',
+    'https://www.groovytunes.com/sleep',
+    'Six hours isn''t enough for anyone!'
+);
